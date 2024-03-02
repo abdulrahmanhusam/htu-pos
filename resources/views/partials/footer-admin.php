f30c96280488ee3af1657b343d4ce6909a9e81f6
abdulrahman.hasan <abdulrahman.h.hasan@gmail.com>
2024-03-03 00:48:04 +0300

    pos

--- resources/views/partials/footer-admin.php
+++ resources/views/partials/footer-admin.php
@@ -5,7 +5,7 @@
 <footer class="bg-dark text-center text-white">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.1);">
-        <p class="m-0">&copy; <?= date('Y') ?> - All Rights Reserved to HTU</p>
+        <p class="m-0">&copy; <?= date('Y') ?> - All Rights Reserved to <a href="https://abdulrahmanhusam.github.io/portfolio/" style="font-family:cursive ">Abdulrahman Hasan</a></p>
     </div>
     <!-- Copyright -->
 </footer>
--- resources/views/partials/footer.php
+++ resources/views/partials/footer.php
@@ -2,6 +2,13 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+<footer class="bg-secondary text-center text-white">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.1);">
+        <p class="m-0">&copy; <?= date('Y') ?> - All Rights Reserved to <a href="https://abdulrahmanhusam.github.io/portfolio/" style="font-family:cursive ">Abdulrahman Hasan</a></p>
+    </div>
+    <!-- Copyright -->
+</footer>
 </body>
 
 </html>
